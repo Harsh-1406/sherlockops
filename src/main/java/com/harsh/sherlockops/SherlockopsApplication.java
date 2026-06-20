e0747700c352e3a9ccb08acea3f6d8aade770f0e
Harsh <hs0033947@gmail.com>
2026-06-20 15:41:23 +0530

    bootstrap spring boot project

---
+++ src/main/java/com/harsh/sherlockops/SherlockopsApplication.java
@@ -0,0 +1,13 @@
+package com.harsh.sherlockops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SherlockopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SherlockopsApplication.class, args);
+	}
+
+}
